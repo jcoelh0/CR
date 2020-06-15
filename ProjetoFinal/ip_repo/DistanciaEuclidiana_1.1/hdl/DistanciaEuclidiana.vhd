fec113c95d58abacfade9f9ccd3e00adffe4aa47
jcoelh0 <jcoelho@ua.pt>
2020-06-15 11:37:09 +0100

    memmory mapped distancia euclidiana

---
+++ ProjetoFinal/ip_repo/DistanciaEuclidiana_1.1/hdl/DistanciaEuclidiana.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/15/2020 11:32:16 AM
+-- Design Name: 
+-- Module Name: DistanciaEuclidiana - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DistanciaEuclidiana is
+    Port ( df : in STD_LOGIC_VECTOR (32 downto 0);
+           result : out STD_LOGIC_VECTOR (32 downto 0));
+end DistanciaEuclidiana;
+
+architecture Behavioral of DistanciaEuclidiana is
+
+begin
+
+
+end Behavioral;
